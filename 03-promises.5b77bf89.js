4938bd60c3a427e66413402eb437fd76893ecaa6
evag2582 <127631774+evag2582@users.noreply.github.com>
2023-10-19 17:45:27 +0000

    Deploying to gh-pages from @ evag2582/goit-js-hw-09@0b183753397609f10bd22bc51acbfc520144fbc0 🚀

---
+++ 03-promises.5b77bf89.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector("input[name='delay']"),document.querySelector("input[name='step']"),document.querySelector("input[name='amount']");document.querySelector(".form").addEventListener("submit",(position,void Math.random()))}();
+//# sourceMappingURL=03-promises.5b77bf89.js.map
---
+++ 03-promises.f8f93131.js
@@ -0,0 +1,2 @@
+document.querySelector("input[name='delay']"),document.querySelector("input[name='step']"),document.querySelector("input[name='amount']");document.querySelector(".form").addEventListener("submit",(position,void Math.random()));
+//# sourceMappingURL=03-promises.f8f93131.js.map
