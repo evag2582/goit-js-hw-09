0b183753397609f10bd22bc51acbfc520144fbc0
evag2582 <elio.g28@gmail.com>
2023-10-19 13:44:52 -0400

    update task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,23 @@
+const delay = document.querySelector("input[name='delay']");
+const delayStep = document.querySelector("input[name='step']");
+const amount = document.querySelector("input[name='amount']");
+const formReg = document.querySelector(".form");
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+   if (shouldResolve) {
+     // Fulfill
+   } else {
+     // Reject
+   }
+ }
+
+ formReg.addEventListener("submit", createPromise(position, delay));
+
+
+  // e.preventDefault();
+  // const {
+  //   elements: { delay, step, amount }
+  // } = e.currentTarget;
+  // console.log(delay.value, step.value, amount.value); 
+
